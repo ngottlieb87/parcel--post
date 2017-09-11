357693c972d9f9f7d85a592fa695d4e7fa1db3ec
john michael and senya webster <senya@>
2017-09-11 13:52:51 -0700

    add specs

--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -1,3 +1,16 @@
 require('rspec')
 require('pry')
 require('parcel')
+
+describe('Parcel') do
+  describe('#volume') do
+    it('will calculate the volume, given three dimensions') do
+      expect(Parcel.new(5,5,5,5).volume).to(eq(125))
+    end
+  end
+  describe('#cost_to_ship') do
+    it('will calculate the cost, given the volume and weight') do
+      expect(Parcel.new(5,5,5,5).cost_to_ship).to(eq(625))
+    end
+  end
+end
